d06edb51b3ebc539afe7a401d2a83160fac3f043
Joel <joelmounts@gmail.com>
2022-05-02 14:27:06 -0600

    added search functions

--- frontend/src/components/restaurants-list.js
+++ frontend/src/components/restaurants-list.js
@@ -1,8 +1,91 @@
 
-import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useState, useEffect } from "react";
+import RestaurantDataService from "../services/restaurant";
+import { Link } from "react-router-dom";
 
-function RestaurantsList() {
+const RestaurantsList = props => {
+    const [restaurants, setRestaurants] = useState([]);
+    const [searchName, setSearchName] = useState("");
+    const [searchZip, setSearchZip] = useState("");
+    const [searchCuisine, setSearchCuisine] = useState("");
+    const [cuisines, setCuisines] = useState(["All Cuisines"]);
+
+    useEffect(() => {
+        retrieveRestaurants();
+        retrieveCuisines();
+    }, []);
+
+    const onChangeSearchName = e => {
+        const searchName = e.target.value;
+        setSearchName(searchName);
+    };
+
+    const onChangeSearchZip = e => {
+        const searchZip = e.target.value;
+        setSearchZip(searchZip);
+    };
+
+    const onChangeSearchCuisine = e => {
+        const searchCuisine = e.target.value;
+        setSearchCuisine(searchCuisine);
+
+    };
+
+    const retrieveRestaurants = () => {
+        RestaurantDataService.getAll()
+            .then(response => {
+                console.log(response.data);
+                setRestaurants(response.data.restaurants);
+
+            })
+            .catch(e => {
+                console.log(e);
+            });
+    };
+
+    const retrieveCuisines = () => {
+        RestaurantDataService.getCuisines()
+            .then(response => {
+                console.log(response.data);
+                setCuisines(["All Cuisines"].concat(response.data));
+
+            })
+            .catch(e => {
+                console.log(e);
+            });
+    };
+
+    const refreshList = () => {
+        retrieveRestaurants();
+    };
+
+    const find = (query, by) => {
+        RestaurantDataService.find(query, by)
+            .then(response => {
+                console.log(response.data);
+                setRestaurants(response.data.restaurants);
+            })
+            .catch(e => {
+                console.log(e);
+            });
+    };
+
+    const findByName = () => {
+        find(searchName, "name")
+    };
+
+    const findByZip = () => {
+        find(searchZip, "zipcode")
+    };
+
+    const findByCuisine = () => {
+        if (searchCuisine == "All Cuisines") {
+            refreshList();
+        } else {
+            find(searchCuisine, "cuisine")
+        }
+    };
+
     return (
         <div className="App">
             its a list!
